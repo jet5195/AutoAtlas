3e169db6642b41eaf6ba83d3a422dd8bca502bce
Jared <jsboleyn@gmail.com>
2017-02-02 11:56:36 -0600

    2nd

---
+++ app/src/main/java/com/example/jsboleyn/autoatlas/myDBHandler.java
@@ -0,0 +1,18 @@
+package com.example.jsboleyn.autoatlas;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MyDBHandler extends SQLiteOpenHelper {
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+}
